9403c7a2930fa8b0edfc8dbbaf0ba2230ce709f6
Youngmin Shin <shinyoungmin@fb.com>
2018-06-27 10:20:14 -0700

    Initial Commit

---
+++ testgitTests/testgitTests.m
@@ -0,0 +1,39 @@
+//
+//  testgitTests.m
+//  testgitTests
+//
+//  Created by Youngmin Shin on 6/27/18.
+//  Copyright © 2018 Youngmin Shin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testgitTests : XCTestCase
+
+@end
+
+@implementation testgitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
